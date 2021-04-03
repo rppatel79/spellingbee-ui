7e470035dc18576bbac14894b024b4f511008ade
Ritesh Patel <ritesh.pri.patel@gmail.com>
2021-04-03 16:44:40 -0700

    fixed typo

--- src/Question.svelte
+++ src/Question.svelte
@@ -7,6 +7,6 @@
 <tr>
     <td>{id}</td>
     <td>
-        <audio controls><source src="audio/{week}}/{word}.mp3" type="audio/mpeg">Your browser does not support the audio element.</audio>
+        <audio controls><source src="audio/{week}/{word}.mp3" type="audio/mpeg">Your browser does not support the audio element.</audio>
     </td>
  </tr>
